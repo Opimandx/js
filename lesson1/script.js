2fbe50441f80242682f4c2e3a6e534fafb0e8ad8
Opimand <silich.iliya@gmail.com>
2018-06-01 15:04:17 +0200

    Первая домашка

---
+++ lesson1/script.js
@@ -0,0 +1,28 @@
+var budget = prompt("Ваш бюджет на месяц : ", 1000)
+var storeName = prompt("Название вашего магазина ", "Котошоп")
+
+mainList = {};  //хотел инициализоровать как в видео, но почему-то не получалось, 
+				//были синтаксические ощибки. Нашел в учебнике такой способ - заработало :с
+
+
+mainList.budget = budget;
+mainList.storeName = storeName;
+mainList.shopGoods = ["first", "second", "third"];
+mainList.employers = {};
+mainList.open = true;
+
+
+mainList.shopGoods[0] = prompt("Введите название первого товара", '')
+mainList.shopGoods[1] = prompt("Введите название второго товара", '')
+mainList.shopGoods[2] = prompt("Введите название третьего товара", '')
+
+ alert(mainList.shopGoods);
+
+
+
+
+ 
+
+
+
+
